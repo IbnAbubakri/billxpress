59867f904ea36c7ce789cb3497da98f9d010d166
IbnAbubakri <faruqsuzay@gmail.com>
2026-07-10 06:40:37 +0100

    Restore api/index.js - Vercel serverless entry point with SQLite init

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import app from '../server/src/app.js';
+import { initDatabase } from '../server/src/utils/db.js';
+import { migrateFromJSON } from '../server/src/utils/migrate.js';
+import seed from '../server/src/seed.js';
+
+let ready = false;
+let readyPromise = null;
+
+export default async function handler(req, res) {
+  if (!ready) {
+    if (!readyPromise) {
+      readyPromise = (async () => {
+        initDatabase();
+        migrateFromJSON();
+        await seed();
+        ready = true;
+      })();
+    }
+    await readyPromise;
+  }
+  app(req, res);
+}
